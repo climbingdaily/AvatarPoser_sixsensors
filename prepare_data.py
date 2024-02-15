7582dd0d8dad90c5d5e57995dd924c22fce1b9cf
climbingdaily <yudidai@stu.xmu.edu.cn>
2024-02-15 19:04:03 +0100

    prepare 30HZ data

--- prepare_data.py
+++ prepare_data.py
@@ -14,6 +14,8 @@ dataroot_amass ="amass" # root of amass dataset
 
 HEAD_POS = 15
 
+
+
 def load_body_models(support_dir='support_data/', num_betas=16, num_dmpls=8):
     # Load SMPL body models (here we load
     # @support_dir, path to the body model directory
@@ -32,128 +34,147 @@ def load_body_models(support_dir='support_data/', num_betas=16, num_dmpls=8):
 
 def load_filepaths(dataroot, subset, phase):
     print(f"Preparing {phase} data")
-    savedir = os.path.join("./data_fps60", dataroot_subset, phase)
-    
-    if not os.path.exists(savedir):
-        os.makedirs(savedir)
 
     split_file = os.path.join(dataroot, subset, phase + "_split.txt")
     try:
         with open(split_file, 'r') as f:
             filepaths = [line.rstrip('\n') for line in f]
-            return filepaths, savedir
+            return filepaths
     except:
         raise ValueError("File not found: {}".format(split_file))
         
 
-for dataroot_subset in ["MPI_HDM05", "BioMotionLab_NTroje", "CMU"]:
-    print(dataroot_subset)
-    for phase in ["train", "test"]:
-        print(f"Preparing {phase} data")
-        
-        filepaths, savedir = load_filepaths("./data_split", dataroot_subset, phase)
-        bm_male, bm_female = load_body_models()
-
-        rotation_local_full_gt_list = []
-        input_joints_params         = []
-        body_parms_list             = []
-        head_global_trans_list      = []
-
-        idx = 0
-        for filepath in filepaths:
-            data  = dict()
-            bdata = np.load(os.path.join('data_split', filepath), allow_pickle=True)
-            # print(list(bdata.keys())) ### check keys of body data: ['trans', 'gender', 'mocap_framerate', 'betas', 'dmpls', 'poses']
-            try:
-                framerate = bdata["mocap_framerate"]
-                print("framerate is {}".format(framerate))
-            except:
-                print(filepath)
-                print(list(bdata.keys()))       
-                continue # skip shape.npz
-
-            idx += 1
-
-            print(idx)
-
-            if framerate == 120:
-                stride = 2
-            elif framerate == 60:
-                stride = 1
-
-            bdata_poses = bdata["poses"][::stride,...]
-            bdata_trans = bdata["trans"][::stride,...]
-            subject_gender = bdata["gender"]
-
-            bm = bm_male # if subject_gender == 'male' else bm_female
-
-            # embed()
-            body_parms = {
-                'root_orient': torch.Tensor(bdata_poses[:, :3]),   #.to(comp_device), # controls the global root orientation
-                'pose_body'  : torch.Tensor(bdata_poses[:, 3:66]), #.to(comp_device), # controls the body
-                'trans'      : torch.Tensor(bdata_trans),          #.to(comp_device), # controls the global body position
-            }
-
-            body_parms_list = body_parms
-
-            # embed()
-            body_pose_world = bm(**{k:v for k,v in body_parms.items() if k in ['pose_body','root_orient','trans']})
-
-            # embed()
-            # self.rotation_local_full_gt_list.append(body_parms['pose_body'])
-            # self.rotation_local_full_gt_list.append(torch.Tensor(bdata['poses'][:, :66]))
+def main(BODDY_JOINTS, out_framerate=60, num_betas=16, num_dmpls=8, support_dir='support_data/', data_root='data_split'):
+    
+    for dataroot_subset in ["MPI_HDM05", "BioMotionLab_NTroje", "CMU"]:
+        print(dataroot_subset)
+        for phase in ["train", "test"]:
+            print(f"Preparing {phase} data")
             
-            # === rotation_local_full_gt_list ===
-            output_aa = torch.Tensor(bdata_poses[:, :66]).reshape(-1,3)
-            output_6d = utils_transform.aa2sixd(output_aa).reshape(bdata_poses.shape[0],-1)
-            rotation_local_full_gt_list = output_6d[1:] # ! shape: (num_frames, 132)
+            savedir = os.path.join(f"data_fps{out_framerate}_J{len(BODDY_JOINTS)}", dataroot_subset, phase)
 
-            # === position_global_full_gt_list ===
-            # ! shape: (num_frames, 22, 3) position of joints relative to the world origin
-            position_full_gt_world  = body_pose_world.Jtr[:,:22,:] 
-            input_position_global   = position_full_gt_world[1:, [15,20,21], :]
-            input_position_relative = position_full_gt_world[1:, [15,20,21], :] - position_full_gt_world[1:, [15,20,21], :]
+            if not os.path.exists(savedir):
+                os.makedirs(savedir)
 
-            # ==== head global transformation ====
-            position_head_world    = position_full_gt_world[:,15,:] # world position of head
-            rotation_local_matrot  = aa2matrot(torch.tensor(bdata_poses).reshape(-1,3)).reshape(bdata_poses.shape[0],-1,9)
-            rotation_global_matrot = local2global_pose(rotation_local_matrot, bm.kintree_table[0].long())
-            head_rot_global_matrot = rotation_global_matrot[:,[15],:,:]
-            head_global_trans      = torch.eye(4).repeat(position_head_world.shape[0],1,1)
-            head_global_trans[:,:3,:3] = head_rot_global_matrot.squeeze()
-            head_global_trans[:,:3,3]  = position_full_gt_world[:,15,:]
-            head_global_trans_list     = head_global_trans[1:] # ! shape: (num_frames, 4, 4)
+            filepaths  = load_filepaths(data_root, dataroot_subset, phase)
+            bm_male, _ = load_body_models()
 
-            # === head, left hand, right hand's global 6d rotation representations ===
-            rotation_global_6d  = utils_transform.matrot2sixd(rotation_global_matrot.reshape(-1,3,3)).reshape(rotation_global_matrot.shape[0],-1,6)
-            input_rot_global_6d = rotation_global_6d[1:,[15,20,21],:] # ! shape: (num_frames, 3, 6) head, left hand, right hand
+            rotation_local_full_gt_list = []
+            input_joints_params         = []
+            body_parms_list             = []
+            head_global_trans_list      = []
 
-            # === head, left hand, right hand's global 6d rotation velocity representations ===
-            rotation_velocity_global_matrot   = torch.matmul(torch.inverse(rotation_global_matrot[:-1]),rotation_global_matrot[1:])
-            rotation_velocity_global_6d       = utils_transform.matrot2sixd(rotation_velocity_global_matrot.reshape(-1,3,3)).reshape(rotation_velocity_global_matrot.shape[0],-1,6)
-            input_rot_velocity_global_6d = rotation_velocity_global_6d[:,[15,20,21],:] # ! shape: (num_frames, 3, 6) head, left hand, right hand
+            idx = 0
+            for filepath in filepaths:
+                data  = dict()
+                bdata = np.load(os.path.join('data_split', filepath), allow_pickle=True)
+                # print(list(bdata.keys())) ### check keys of body data: ['trans', 'gender', 'mocap_framerate', 'betas', 'dmpls', 'poses']
+                try:
+                    framerate = bdata["mocap_framerate"]
+                    print(f"Original framerate: {framerate:.0f} | {filepath}", end = " ")
+                except:
+                    print(filepath)
+                    print(list(bdata.keys()))       
+                    continue # skip shape.npz
 
-            assert input_rot_global_6d.shape[0] == input_rot_velocity_global_6d.shape[0] == input_position_global.shape[0] == input_position_relative.shape[0], "shape mismatch"
+                idx += 1
 
-            num_frames = input_rot_global_6d.shape[0]
+                stride = max(int(framerate // out_framerate), 1)
 
-            input_joints_params = torch.cat([input_rot_global_6d.reshape(num_frames,-1),              # ! shape: (num_frames, 3*6) 
-                                             input_rot_velocity_global_6d.reshape(num_frames,-1),     # ! shape: (num_frames, 3*6)
-                                             input_position_global.reshape(num_frames,-1),            # ! shape: (num_frames, 3*3) 
-                                             input_position_relative.reshape(num_frames,-1)], dim=-1) # ! shape: (num_frames, 3*3)
+                bdata_poses = bdata["poses"][::stride,...]
+                bdata_trans = bdata["trans"][::stride,...]
+                subject_gender = bdata["gender"]
 
-            # training data
-            data['rotation_local_full_gt_list'] = rotation_local_full_gt_list # local rotation,  (num_frames, 22*6)
-            data['input_joints_params']         = input_joints_params         # global position, (num_frames, 22*3)
-            data['body_parms_list']             = body_parms_list             # input data,  including global orientation, rotation velocity, global translation, relative translation
-            data['head_global_trans_list']      = head_global_trans_list      # head global transformation (num_frames, 4, 4)
+                bm = bm_male # if subject_gender == 'male' else bm_female
 
-            # data properties
-            data['framerate'] = 60
-            data['gender']    = subject_gender
-            data['filepath']  = filepath
+                # embed()
+                body_parms = {
+                    'root_orient': torch.Tensor(bdata_poses[:, :3]),   #.to(comp_device), # controls the global root orientation
+                    'pose_body'  : torch.Tensor(bdata_poses[:, 3:66]), #.to(comp_device), # controls the body
+                    'trans'      : torch.Tensor(bdata_trans),          #.to(comp_device), # controls the global body position
+                }
 
-            print(str(idx)+'/'+str(len(filepaths)))
-            #embed()
-            with open(os.path.join(savedir,'{}.pkl'.format(idx)), 'wb') as f:
-                pickle.dump(data, f)
+                body_parms_list = body_parms
+
+                # embed()
+                body_pose_world = bm(**{k:v for k,v in body_parms.items() if k in ['pose_body','root_orient','trans']})
+
+                # embed()
+                # self.rotation_local_full_gt_list.append(body_parms['pose_body'])
+                # self.rotation_local_full_gt_list.append(torch.Tensor(bdata['poses'][:, :66]))
+                
+                # === rotation_local_full_gt_list ===
+                output_aa = torch.Tensor(bdata_poses[:, :66]).reshape(-1,3)
+                output_6d = utils_transform.aa2sixd(output_aa).reshape(bdata_poses.shape[0],-1)
+                rotation_local_full_gt_list = output_6d[1:] # ! shape: (num_frames, 132)
+
+                # === position_global_full_gt_list ===
+                # ! shape: (num_frames, 22, 3) position of joints relative to the world origin
+                position_full_gt_world  = body_pose_world.Jtr[:,:22,:] 
+                input_position_global   = position_full_gt_world[1:, BODDY_JOINTS, :]
+                input_position_relative = position_full_gt_world[1:, BODDY_JOINTS, :] - position_full_gt_world[1:, BODDY_JOINTS, :]
+
+                # ==== head global transformation ====
+                position_head_world    = position_full_gt_world[:,15,:] # world position of head
+                rotation_local_matrot  = aa2matrot(torch.tensor(bdata_poses).reshape(-1,3)).reshape(bdata_poses.shape[0],-1,9)
+                rotation_global_matrot = local2global_pose(rotation_local_matrot, bm.kintree_table[0].long())
+                head_rot_global_matrot = rotation_global_matrot[:,[15],:,:]
+                head_global_trans      = torch.eye(4).repeat(position_head_world.shape[0],1,1)
+                head_global_trans[:,:3,:3] = head_rot_global_matrot.squeeze()
+                head_global_trans[:,:3,3]  = position_full_gt_world[:,15,:]
+                head_global_trans_list     = head_global_trans[1:] # ! shape: (num_frames, 4, 4)
+
+                # === head, left hand, right hand's global 6d rotation representations ===
+                rotation_global_6d  = utils_transform.matrot2sixd(rotation_global_matrot.reshape(-1,3,3)).reshape(rotation_global_matrot.shape[0],-1,6)
+                input_rot_global_6d = rotation_global_6d[1:,BODDY_JOINTS,:] # ! shape: (num_frames, 3, 6) head, left hand, right hand
+
+                # === head, left hand, right hand's global 6d rotation velocity representations ===
+                rotation_velocity_global_matrot   = torch.matmul(torch.inverse(rotation_global_matrot[:-1]),rotation_global_matrot[1:])
+                rotation_velocity_global_6d       = utils_transform.matrot2sixd(rotation_velocity_global_matrot.reshape(-1,3,3)).reshape(rotation_velocity_global_matrot.shape[0],-1,6)
+                input_rot_velocity_global_6d = rotation_velocity_global_6d[:,BODDY_JOINTS,:] # ! shape: (num_frames, 3, 6) head, left hand, right hand
+
+                assert input_rot_global_6d.shape[0] == input_rot_velocity_global_6d.shape[0] == input_position_global.shape[0] == input_position_relative.shape[0], "shape mismatch"
+
+                num_frames = input_rot_global_6d.shape[0]
+
+                input_joints_params = torch.cat([input_rot_global_6d.reshape(num_frames,-1),              # ! shape: (num_frames, J*6) 
+                                                input_rot_velocity_global_6d.reshape(num_frames,-1),     # ! shape: (num_frames, J*6)
+                                                input_position_global.reshape(num_frames,-1),            # ! shape: (num_frames, J*3) 
+                                                input_position_relative.reshape(num_frames,-1)], dim=-1) # ! shape: (num_frames, J*3)
+
+                # training data
+                data['rotation_local_full_gt_list'] = rotation_local_full_gt_list # local rotation,  (num_frames, 22*6)
+                data['input_joints_params']         = input_joints_params         # global position, (num_frames, 22*3)
+                data['body_parms_list']             = body_parms_list             # input data,  including global orientation, rotation velocity, global translation, relative translation
+                data['head_global_trans_list']      = head_global_trans_list      # head global transformation (num_frames, 4, 4)
+
+                # data properties
+                data['framerate'] = 60
+                data['gender']    = subject_gender
+                data['filepath']  = filepath
+
+                print(str(idx)+'/'+str(len(filepaths)))
+                #embed()
+                with open(os.path.join(savedir,'{}.pkl'.format(idx)), 'wb') as f:
+                    pickle.dump(data, f)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--frame_rate", type=int, default=60)
+    parser.add_argument("--num_betas", type=int, default=16)
+    parser.add_argument("--num_dmpls", type=int, default=8)
+    parser.add_argument("--support_dir", type=str, default='support_data/')
+    parser.add_argument("--joint_type", type=int, default=0, 
+                        help="0: root, left ankle, right ankle, head, left hand, right hand; 1: head, left hand, right hand")
+
+    args = parser.parse_args()
+
+    if args.joint_type == 0:
+        BODDY_JOINTS = [0,7,8,15,20,21]
+    elif args.joint_type == 1:
+        BODDY_JOINTS = [15,20,21]
+    else:
+        raise ValueError("joint_type should be 0 or 1")
+
+    main(BODDY_JOINTS, args.frame_rate, args.num_betas, args.num_dmpls, args.support_dir)
